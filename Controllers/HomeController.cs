c003130f31fea0a7f5606ba31b5be7eab535bce2
vasif123 <vasifaha@code.edu.az>
2022-07-12 03:05:31 +0400

    first

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController: Controller
+    {
+        public IActionResult Index()
+        {
+            Testimonial model = new Testimonial { 
+            Id=1,
+            Maintext= "Testimonials",
+            Maintitle= "Necessitatibus eius consequatur ex aliquid fuga eum quidem",
+            Divtext= " Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam. ",
+            person="Murad Abdullayev",
+            Image= "assets/img/testimonials/testimonials-6.jpg",
+            Imagetext= "Programmer"
+
+            };
+            return View(model);
+        }
+    }
+}
---
+++ Models/Testimonial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Testimonial
+    {
+        public int Id { get; set; }
+        public string Maintext { get; set; }
+        public string Maintext2 { get; set; }
+        public string Maintitle { get; set; }
+        public string Divtext { get; set; }
+        public string person { get; set; }
+        public string person1 { get; set; }
+        public string Image { get; set; }
+        public string Imagetext { get; set; }
+        public string Image1 { get; set; }
+
+        public string Image2 { get; set; }
+
+    }
+}
